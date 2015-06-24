62255889928224127faa0a12d78671093fd39727
Jesse <microji@126.com>
2015-06-24 22:34:10 +0800

    beautify code

--- mz.rb
+++ mz.rb
@@ -36,18 +36,14 @@ end
 class FetchLite
 	
 	def initialize(opt={})
-		trap("INT") do 
-			puts " Interrupt, Exit." 
-			exit 130
-		end
 		ENV['http_proxy'] = ENV['https_proxy'] = "http://127.0.0.1:8087" if opt[:proxy]
 		@agent = opt['agent'] || 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0'
+		@open_opt = {'User-Agent'=>@agent, :allow_redirections=>:safe}.merge(opt[:open_opt] || {})
 	end
 	
 	def fetch_page_image(url, opt)
-		opn = {'User-Agent'=>@agent, :allow_redirections=>:safe}
 		begin
-			num = opt[:end] || open(url, opn).read.scan(opt[:fn_page]).flatten[0].to_i
+			num = opt[:end] || open(url, @open_opt).read.scan(opt[:fn_page]).flatten[0].to_i
 		rescue => e
 			abort "url: #{url}, error: #{e.message}"
 		end
@@ -57,7 +53,7 @@ class FetchLite
 		str.upto(num) do |p|
 			u = url.is_a?(Array) ? url[p-1] : url+(nxt ? (nxt.empty? ? '' : opt[:page_url] % nxt) : opt[:page_url] % p)
 			begin 
-				htm = open(u, opn).read
+				htm = open(u, @open_opt).read
 				doc = Nokogiri::HTML.parse(opt[:json_key] ? JSON.parse(htm)[opt[:json_key]] : htm) 
 			rescue => e
 				puts "url: #{u}, error: #{e.message}" 
@@ -70,7 +66,7 @@ class FetchLite
 				file = opt[:fn_name] ? opt[:save_path] % File.basename(img).sub(opt[:fn_name],'') : opt[:save_path] % [p,imgs.size,i+1,File.extname(img)]
 				found += 1 if File.exists?(file)
 				begin
-					fnum += 1 if !File.exists?(file) && IO.write(file,open(img, opn).read) > 0 
+					fnum += 1 if !File.exists?(file) && IO.write(file,open(img, @open_opt).read) > 0 
 				rescue => e
 					puts "img: #{img} , #{e.message}"
 				end
@@ -93,16 +89,16 @@ class FetchLite
 	end 
 
 	def archive(url, opt)
-		opn = {'User-Agent'=>@agent, :allow_redirections=>:safe}
+		result, start = [], Time.now 
 		begin
-			result, start, mon = [], Time.now, Nokogiri::HTML.parse(open(url, opn)).css(opt[:month_query])
+			mon = Nokogiri::HTML.parse(open(url, @open_opt)).css(opt[:month_query])
 		rescue => e
 			abort "url: #{url}, error: #{e.message}"
 		end
 		Parallel.each(mon, :in_threads=>[opt[:threads] || 10, mon.size].min) do |m|
 			begin 
 				istart, u = Time.now, m.attr('href') 
-				v = open(opt[:list_url] % URI.encode(u, /\W/), opn).read
+				v = open(opt[:list_url] % URI.encode(u, /\W/), @open_opt).read
 				v = v.scan(opt[:result_reg])[0][0] if opt[:result_reg] && v
 				b = YAML.load(v)[opt[:result_key]] if v
 				s = b.map{|l| opt[:save_fmt] % l.symbolize_keys!} if b
@@ -147,58 +143,56 @@ class FetchLite
 	end
 end
 
-wch = ARGV[0] if ARGV[0] 
-abort "must set argv which" unless wch
+trap("INT") do abort " Interrupt, Exit." end
+abort "must set argv which" unless ARGV[0]
 opt = {} 
 argvs_str = ARGV.grep(/^str=[1-9][0-9]*$/)[0]
 argvs_end = ARGV.grep(/^end=[1-9][0-9]*$/)[0]
 opt[:str] = argvs_str[4..-1].to_i if argvs_str
 opt[:end] = argvs_end[4..-1].to_i if argvs_end
 
-case wch 
+case ARGV[0] 
 when "mzt"
-	url = "http://www.mzitu.com/share/"
 	abort "need save dir argument" unless ARGV[1] && Dir.exists?(ARGV[1])	
-	opt.merge!({:fn_page=>%r|<span class='page-numbers current'>(\d+)</span>|, :page_url=>"comment-page-%d", :threads=>20,
-	   :img_query=>".commentlist img", :save_path=>"./#{ARGV[1].chomp('/')}/%03d-%02d-%02d%s"})
+	url = "http://www.mzitu.com/share/"
+	opt.merge! fn_page: %r|<span class='page-numbers current'>(\d+)</span>|, page_url: "comment-page-%d", threads: 20,
+						 img_query: ".commentlist img", save_path: "./#{ARGV[1].chomp('/')}/%03d-%02d-%02d%s"
 	FetchLite.new.fetch_page_image url, opt
 when "dbf"
-	url = "http://www.doubanfuli.com/"
 	abort "need save dir argument" unless ARGV[1] && Dir.exists?(ARGV[1])	
-	opt.merge!({:fn_page=>%r|<a href='.*/(\d+)'>最后</a>|, :page_url=>"page/%d", :threads=>20,
-	   :img_query=>"article img[src*=timthumb]", :fn_img=>/src=([^&]+)/, :save_path=>"./#{ARGV[1].chomp('/')}/%03d-%02d-%02d%s"})
+	url = "http://www.doubanfuli.com/"
+	opt.merge! fn_page: %r|<a href='.*/(\d+)'>最后</a>|, page_url: "page/%d", threads: 20,
+						 img_query: "article img[src*=timthumb]", fn_img: /src=([^&]+)/, save_path: "./#{ARGV[1].chomp('/')}/%03d-%02d-%02d%s"
 	FetchLite.new.fetch_page_image url, opt
 when "url"
 	abort "need file argument" unless ARGV[1] 
 	abort "need save dir argument" unless ARGV[2] && Dir.exists?(ARGV[2])
-	opt.merge!({:threads=>20, :img_query=>".post-body img[src^=http]", :save_path=>"#{ARGV[2].chomp('/')}/%02d-%03d-%03d%s"})
+	opt.merge! threads: 20, img_query: ".post-body img[src^=http]", save_path: "#{ARGV[2].chomp('/')}/%02d-%03d-%03d%s"
 	FetchLite.new(:proxy=>true).fetch_urls_image ARGV[1], opt
 when "twt"
 	abort "need user argument" unless ARGV[1]
 	abort "need save dir argument" unless ARGV[2] && Dir.exists?(ARGV[2])
 	url = "https://twitter.com/i/profiles/show/#{ARGV[1]}/media_timeline?include_available_features=1&include_entities=1&last_note_ts=1943"
-	opt.merge!({:img_query=>".media-thumbnail", :attr=>'data-url', :end=>Float::INFINITY, 
-				:next_page=>/stream-item-tweet-(\d+)/, :next_index=>-1, :page_url=>'&max_position=%s', :json_key=>'items_html',
-				:save_path=>"#{ARGV[2].chomp('/')}/%s", :fn_name=>/:\w+$/, :force_go=>true})
+	opt.merge! img_query: ".media-thumbnail", attr: 'data-url', end: Float::INFINITY, 
+						 next_page: /stream-item-tweet-(\d+)/, next_index: -1, page_url: '&max_position=%s', json_key: 'items_html',
+						 save_path: "#{ARGV[2].chomp('/')}/%s", fn_name: /:\w+$/, force_go: ARGV[3] == 'force_go'
 	FetchLite.new(:proxy=>true).fetch_page_image url, opt 
 when "arc"
 	abort "need site argument" unless ARGV[1] 
 	abort "need save file argument" unless ARGV[2] 
 	url = "http://#{ARGV[1]}.blogspot.com/"
-	opt.merge!({:month_query=>'#BlogArchive1_ArchiveList a.post-count-link[href*=archive]', 
-				:list_url=>"#{url}?action=getTitles&widgetId=BlogArchive1&widgetType=BlogArchive&responseType=js&path=%s",
-				:result_reg=> /'getTitles',(.*)\);\n\} catch/, :result_key=>"posts", :threads=>10, 
-				:save_fmt=>"%{url}\t%{title}", :save_file=>ARGV[2] })
+	opt.merge! month_query: '#BlogArchive1_ArchiveList a.post-count-link[href*=archive]', 
+						 list_url: "#{url}?action=getTitles&widgetId=BlogArchive1&widgetType=BlogArchive&responseType=js&path=%s",
+						 result_reg: /'getTitles',(.*)\);\n\} catch/, result_key: "posts", threads: 10, 
+						 save_fmt: "%{url}\t%{title}", save_file: ARGV[2]
 	FetchLite.new(:proxy=>true).archive url, opt	
 when "flt"
 	abort "need from file param" unless ARGV[1]
 	abort "need to file param" unless ARGV[2]
-	opt = {:url_refine=>/\d{4}\/\d{2}/, :sug_match=>/^(?!.*MKV).*\[\d+P\].*$/i, 
-		   :exclude=>%w(13/10/15p.html 13/10/9p-36p.html heros12p 13/11/b29p.html motorbike 15/04/28p.html 15/04/29p.html 15/04/31p.html), 
-		   :auto => ARGV[3] == '-' ? true : false }
+	opt = {url_refine: /\d{4}\/\d{2}/, sug_match: /^(?!.*MKV).*\[\d+P\].*$/i, 
+		     exclude: %w(13/10/15p.html 13/10/9p-36p.html heros12p 13/11/b29p.html motorbike 15/04/28p.html 15/04/29p.html 15/04/31p.html), 
+				 auto: ARGV[3] == '-' ? true : false }
 	FetchLite.new.filter_urls ARGV[1], ARGV[2], opt
 else
 	puts "nothing to do"
 end 
-
-
